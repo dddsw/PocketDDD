d6bcd7e6e36ed9f569696f8015f836969b8c740e
Russell Day <russell.day.coder@gmail.com>
2024-04-21 11:20:23 +0100

    Add auth using

--- PocketDDD.Server/PocketDDD.Server.WebAPI/Controllers/RegistrationController.cs
+++ PocketDDD.Server/PocketDDD.Server.WebAPI/Controllers/RegistrationController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PocketDDD.Server.Model.DTOs;
 using PocketDDD.Server.Services;
