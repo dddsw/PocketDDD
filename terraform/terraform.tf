cf28dee3c7f0fc159b64359db36927be47de62aa
Russell Day <russell.day.coder@gmail.com>
2024-04-23 22:58:08 +0100

    Remove other GitHub related bits out of terraform

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.100.0"
     }
-    github = {
-      source = "integrations/github"
-      version = "6.2.1"
-    }
     random = {
       source  = "hashicorp/random"
       version = "3.6.1"
@@ -24,11 +20,5 @@ provider "azurerm" {
   # Configuration options
   features {
 
-  }
-}
-
-provider "github" {
-  owner = "dddsw"
-  app_auth {
   }
 }
