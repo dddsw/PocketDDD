1d3f0b9b0a1b18aeb40b09a3c19f85375f21d895
Russell Day <russell.day.coder@gmail.com>
2024-04-23 22:18:26 +0100

    Pls fix

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -30,8 +30,5 @@ provider "azurerm" {
 provider "github" {
   owner = "dddsw"
   app_auth {
-    id              = var.app_id              # or `GITHUB_APP_ID`
-    installation_id = var.app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
-    pem_file        = var.app_pem_file        # or `GITHUB_APP_PEM_FILE`
   }
 }
