23f221c98ecd12b53554aa63a8e7885851e1d156
Russell Day <russell.day.coder@gmail.com>
2024-04-23 22:16:25 +0100

    Auth with GitHub another way

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -29,4 +29,9 @@ provider "azurerm" {
 
 provider "github" {
   owner = "dddsw"
+  app_auth {
+    id              = var.app_id              # or `GITHUB_APP_ID`
+    installation_id = var.app_installation_id # or `GITHUB_APP_INSTALLATION_ID`
+    pem_file        = var.app_pem_file        # or `GITHUB_APP_PEM_FILE`
+  }
 }
