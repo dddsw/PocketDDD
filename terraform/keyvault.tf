1cad88bd766b8b34687fcf2a6b9c8fda9a56bc81
Russell Day <russell.day.coder@gmail.com>
2024-04-23 21:50:57 +0100

    Try new keyvault name

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "key_vault" {
-  name                        = "${local.resource_prefix}-secrets-keyvault"
+  name                        = "${local.resource_prefix}-secrets-kv"
   location                    = azurerm_resource_group.rg.location
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
