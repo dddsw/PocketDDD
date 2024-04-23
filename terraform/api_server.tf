f469451055e8d69af961a169674aa6048d56fa0a
Russell Day <russell.day.coder@gmail.com>
2024-04-23 21:49:15 +0100

    Choose different names for name-conflicted resources

--- terraform/api_server.tf
+++ terraform/api_server.tf
@@ -8,7 +8,7 @@ resource "azurerm_service_plan" "api_server_service_plan" {
 
 
 resource "azurerm_linux_web_app" "api_server_web_app" {
-  name                = "${local.resource_prefix}-api-server"
+  name                = "${local.resource_prefix}-api-server-web-app"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_service_plan.api_server_service_plan.location
   service_plan_id     = azurerm_service_plan.api_server_service_plan.id
