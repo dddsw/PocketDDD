bdd7fc6b3fc3ed0412534bef7aec0f93841bec6d
Russell Day <russell.day.coder@gmail.com>
2024-04-25 23:52:18 +0100

    Add output of the client custom domain

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "api_server_url" {
   value = "https://${azurerm_linux_web_app.api_server_web_app.default_hostname}/"
 }
 
-# output "client_app_public_url" {
-#   value = 
-# }
+output "client_app_public_url" {
+  value = "https://${azurerm_static_web_app_custom_domain.custom_domain.domain_name}"
+}
