e430893906baabf67890cad6e2e566eff04d7610
Russell Day <russell.day.coder@gmail.com>
2024-04-25 22:25:09 +0100

    Read from data

--- terraform/blazor_client.tf
+++ terraform/blazor_client.tf
@@ -28,7 +28,7 @@ data "cloudflare_zone" "dns_zone" {
 }
 
 resource "cloudflare_record" "example" {
-  zone_id = cloudflare_zone.dns_zone.id
+  zone_id = data.cloudflare_zone.dns_zone.id
   name    = local.subdomain
   value   = azurerm_static_web_app.blazor-client.default_host_name
   type    = "CNAME"
