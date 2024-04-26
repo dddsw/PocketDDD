0a7118178784bdbcfcec82e60a6be1f9999e64f9
Russell Day <russell.day.coder@gmail.com>
2024-04-26 14:53:30 +0100

    Change prod subdomain to pocket2024

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,5 @@
 locals {
   resource_prefix = "pocketddd-${var.env}"
   sql_server_name = "${local.resource_prefix}-sql-server"
-  subdomain = var.env == "production" ? "pocket" : "pocket-${var.env}"
+  subdomain = var.env == "pocket2024" ? "pocket" : "pocket-${var.env}"
 }
