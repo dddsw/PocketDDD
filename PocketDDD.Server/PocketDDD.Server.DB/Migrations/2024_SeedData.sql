b24544b989008f56aedeb87301df8afc6bc8a3f4
Sara Gowen <me@dynamictulip.com>
2024-04-26 12:15:56 +0100

    Add disambiguation around reseed identity for EventDetail

--- PocketDDD.Server/PocketDDD.Server.DB/Migrations/2024_SeedData.sql
+++ PocketDDD.Server/PocketDDD.Server.DB/Migrations/2024_SeedData.sql
@@ -12,10 +12,13 @@ delete EventDetail
 GO
 
 -- Reset the identity columns
-DBCC CHECKIDENT ('[EventDetail]', RESEED, 1); -- There is hardcoding to EventDetail ID 1 so we reset to 1 not 0
 DBCC CHECKIDENT ('[Tracks]', RESEED, 0);
 DBCC CHECKIDENT ('[TimeSlots]', RESEED, 0);
 DBCC CHECKIDENT ('[Sessions]', RESEED, 0);
+
+-- There is hardcoding to EventDetail ID 1 so we reset to 1 not 0
+DBCC CHECKIDENT ('[EventDetail]', RESEED, 1); -- Use if this is a brand new table that has never been used before
+--DBCC CHECKIDENT ('[EventDetail]', RESEED, 0); -- Use if this is an empty table that used to have rows
 
 GO
 
