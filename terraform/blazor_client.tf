c3eacd109a459336448c4e01f2f0e2446d6b3008
Russell Day <russell.day.coder@gmail.com>
2025-04-07 22:40:37 +0100

    Accomodate existing cloudflare record

--- terraform/blazor_client.tf
+++ terraform/blazor_client.tf
@@ -25,16 +25,21 @@ data "cloudflare_zone" "dns_zone" {
   name       = "dddsouthwest.com"
 }
 
-resource "cloudflare_record" "cname_record" {
-  zone_id = data.cloudflare_zone.dns_zone.id
-  name    = local.subdomain
-  content = azurerm_static_web_app.blazor-client.default_host_name
-  type    = "CNAME"
-  ttl     = 3600
+# resource "cloudflare_record" "cname_record" {
+#   zone_id = data.cloudflare_zone.dns_zone.id
+#   name    = local.subdomain
+#   content = azurerm_static_web_app.blazor-client.default_host_name
+#   type    = "CNAME"
+#   ttl     = 3600
+# }
+
+data "cloudflare_record" "cname_record" {
+  zone_id  = data.cloudflare_zone.dns_zone.id
+  hostname = azurerm_static_web_app.blazor-client.default_host_name
 }
 
 resource "azurerm_static_web_app_custom_domain" "custom_domain" {
   static_web_app_id = azurerm_static_web_app.blazor-client.id
-  domain_name       = "${cloudflare_record.cname_record.name}.${data.cloudflare_zone.dns_zone.name}"
+  domain_name       = "${data.cloudflare_record.cname_record.hostname}"
   validation_type   = "cname-delegation"
 }
