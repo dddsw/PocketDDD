0dcfd9bc6ee0a4b2f6fe7aae33e846240a2de951
Russell Day <russell.day.coder@gmail.com>
2025-04-15 22:08:44 +0100

    Fix deployments (#21)

--- terraform/blazor_client.tf
+++ terraform/blazor_client.tf
@@ -28,13 +28,13 @@ data "cloudflare_zone" "dns_zone" {
 resource "cloudflare_record" "cname_record" {
   zone_id = data.cloudflare_zone.dns_zone.id
   name    = local.subdomain
-  value   = azurerm_static_web_app.blazor-client.default_host_name
+  content = azurerm_static_web_app.blazor-client.default_host_name
   type    = "CNAME"
   ttl     = 3600
 }
 
 resource "azurerm_static_web_app_custom_domain" "custom_domain" {
   static_web_app_id = azurerm_static_web_app.blazor-client.id
-  domain_name       = "${cloudflare_record.cname_record.name}.${data.cloudflare_zone.dns_zone.name}"
+  domain_name       = "${cloudflare_record.cname_record.hostname}"
   validation_type   = "cname-delegation"
 }
