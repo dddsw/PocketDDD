847bc8694b1fb5684e858a47a375fa9ddc9dca40
Russell Day <russell.day3@nhs.net>
2026-05-08 22:13:42 +0100

    Reinstate updated cloudflare resources

--- terraform/blazor_client.tf
+++ terraform/blazor_client.tf
@@ -7,7 +7,7 @@ resource "azurerm_static_web_app" "blazor-client" {
   sku_size = var.client_sku_size
 
   app_settings = {
-    "apiUrl" : "https://pocketddd-${var.env}-api-server-web-app.azurewebsites.net/api/"
+    "apiUrl" : "https://${azurerm_linux_web_app.api_server_web_app.default_hostname}/api/"
     "fakeBackend" : "false"
   }
 
@@ -20,21 +20,21 @@ resource "azurerm_key_vault_secret" "blazor_client_deployment_token" {
   key_vault_id = azurerm_key_vault.key_vault.id
 }
 
-# data "cloudflare_zone" "dns_zone" {
-#   account_id = var.cloudflare_account_id
-#   name       = "dddsouthwest.com"
-# }
+data "cloudflare_zone" "dns_zone" {
+  account_id = var.cloudflare_account_id
+  name       = "dddsouthwest.com"
+}
 
-# resource "cloudflare_record" "cname_record" {
-#   zone_id = data.cloudflare_zone.dns_zone.id
-#   name    = local.subdomain
-#   content = azurerm_static_web_app.blazor-client.default_host_name
-#   type    = "CNAME"
-#   ttl     = 3600
-# }
+resource "cloudflare_dns_record" "cname_record" {
+  zone_id = data.cloudflare_zone.dns_zone.id
+  name    = local.subdomain
+  content = azurerm_static_web_app.blazor-client.default_host_name
+  type    = "CNAME"
+  ttl     = 3600
+}
 
-# resource "azurerm_static_web_app_custom_domain" "custom_domain" {
-#   static_web_app_id = azurerm_static_web_app.blazor-client.id
-#   domain_name       = cloudflare_record.cname_record.hostname
-#   validation_type   = "cname-delegation"
-# }
+resource "azurerm_static_web_app_custom_domain" "custom_domain" {
+  static_web_app_id = azurerm_static_web_app.blazor-client.id
+  domain_name       = cloudflare_record.cname_record.hostname
+  validation_type   = "cname-delegation"
+}
